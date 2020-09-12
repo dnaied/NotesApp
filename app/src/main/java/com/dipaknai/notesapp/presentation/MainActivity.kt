7c070aa0b3e635ca78ddf9c725ca082c78159d82
Dipak Nai <itsdipaknai@gmail.com>
2020-09-12 08:40:57 +0530

    Project Setup

---
+++ app/src/main/java/com/dipaknai/notesapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dipaknai.notesapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dipaknai.notesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
